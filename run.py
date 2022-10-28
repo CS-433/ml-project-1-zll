833151141381ac0e42d0f283d3f9d99605a8e766
JaineLi <zhan.li@epfl.ch>
2022-10-28 22:07:06 +0200

    commit

--- run.py
+++ run.py
@@ -12,63 +12,52 @@ def main():
     y_train, tx_train, ids_train = load_csv_data("train.csv")
     _, tx_test, ids_test = load_csv_data("test.csv")
     print("Data loading done.")
-    
-    
-    # data preprocessing 
+
+    # data preprocessing
     # extract usable data by replacing -999 in feature matrix with mean of this feature
     tx_train = fix_null_value(tx_train)
     tx_test = fix_null_value(tx_test)
 
-
     # delete feature columns with correlation larger than 0.75
     corr = np.corrcoef(tx_train.T)
-    mask = np.zeros_like(corr, dtype = bool)
+    mask = np.zeros_like(corr, dtype=bool)
     mask[np.triu_indices_from(mask)] = True
     corr[mask] = 0
     delete_index = list(set(np.where(np.abs(corr) >= 0.75)[0]))
     tx_train = delete_related_features(tx_train, delete_index)
     tx_test = delete_related_features(tx_test, delete_index)
 
-
-    # data preprocessing -- mean subtraction and normalization   
-    tx_train = (tx_train - np.mean(tx_train,axis=0)) / np.std(tx_train,axis=0)
-    tx_test =  (tx_test - np.mean(tx_test,axis=0)) / np.std(tx_test,axis=0)
-
+    # data preprocessing -- mean subtraction and normalization
+    tx_train = (tx_train - np.mean(tx_train, axis=0)) / np.std(tx_train, axis=0)
+    tx_test = (tx_test - np.mean(tx_test, axis=0)) / np.std(tx_test, axis=0)
 
     # data preprocessing -- build polynomial extension for input features
     degree = 3
     tx_train = build_poly(tx_train, degree)
     tx_test = build_poly(tx_test, degree)
 
-
     # add offset to the feature matrix tx_train and tx_test
     tx_train = np.c_[np.ones((tx_train.shape[0], 1)), tx_train]
     tx_test = np.c_[np.ones((tx_test.shape[0], 1)), tx_test]
     print("Data preprocessing done.")
 
-
     # map labels from {-1, 1} to {0, 1} in order to apply logistic regression
     y_train[np.where(y_train == -1)] = 0
 
-
     # set the hyper-parameters
     lambda_ = 1e-5
-
 
     # choose the best method to obtain predicted labels of our test set --- CHOOSE RIDGE REGRESSION HERE
     w, loss = ridge_regression(y_train, tx_train, lambda_)
     y_test = predict_labels(w, tx_test)
 
-
     # map the predicted labels back to {-1, 1}
     y_test[np.where(y_test == 0)] = -1
     print("Label prediction done.")
 
-    
     # transform predicted labels into csv file for submission
     create_csv_submission(ids_test, y_test, "submission_binary_classification.csv")
 
-    
 
 if __name__ == "__main__":
     main()
